e2a88aedce09192ddbe2897532d763d6246308c5
Kim Jong IL <jngkim@amazon.com>
2021-09-17 14:15:55 +0900

    Splash Container

--- App.tsx
+++ App.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from "./components/Splash";
+import Home from "./components/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Splash />;
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Splash" component={Splash} options={{headerShown:false}}/>
+        <Stack.Screen name="Home" component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  //  <Splash />;
 }
--- components/Splash.tsx
+++ components/Splash.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Image, View } from "react-native";
+
+import { Image, View, Button } from "react-native";
 import { SplashContainer, SplashLogo } from "../styles/appStyles";
 
-const Splash = () => {
+const Splash = ({ navigation }) => {
+
+  useEffect(() => {
+    setTimeout( () => {
+      navigation.navigate('Home');
+    }, 3000);   
+  }, []);
+
   return (
     <SplashContainer>
       <SplashLogo source={require("../assets/splash_logo.png")} />
